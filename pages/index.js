b43adc3bac600db32de9f82d4c0681353bf20cf9
pratik mane <pratik.mane1299@gmail.com>
2021-04-21 21:10:48 +0530

    Fetch single post details and display

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import Head from 'next/head'
 import { QueryClient, QueryClientProvider, useQuery } from 'react-query';
 import axios from 'axios';
@@ -7,6 +8,7 @@ import styles from '../styles/Home.module.css'
 const queryClient = new QueryClient();
 
 export default function Home() {
+  const [activePostId, setActivePostId] = useState();
   return (
     <QueryClientProvider client={queryClient}>
       <div className={styles.container}>
@@ -23,7 +25,11 @@ export default function Home() {
             <span>Sidebar</span>
           </div>
           <main className={styles.main}>
-            <Posts />
+            {activePostId ? (
+              <Post postId={activePostId} setActivePostId={setActivePostId} /> 
+            ) : (
+              <Posts setActivePostId={setActivePostId} />
+            )}
           </main>
           <footer className={styles.footer}>
             <span>Made with 💜</span>
@@ -38,7 +44,7 @@ function fetchPosts(key) {
   return axios.get('/api/posts').then(res => res.data);
 }
 
-function Posts() {
+function Posts({ setActivePostId }) {
 
   const { status, data, error, isFetching } = useQuery('posts', fetchPosts);
 
@@ -56,10 +62,41 @@ function Posts() {
           </h3>
           <div className={styles.postsList}>
             {data.map((post) => (
-              <div className={styles.card} key={post.id}>
+              <div className={styles.card} key={post.id} onClick={() => {
+                setActivePostId(post.id)
+              }}>
                 <a href="#">{post.title}</a>
               </div>
             ))}
+          </div>
+        </>
+      )}
+    </>
+  );
+}
+
+function fetchPost({ queryKey }) {
+  return axios.get(`/api/posts/${queryKey[1]}`).then(res => res.data);
+}
+
+function Post({ postId, setActivePostId }) {
+  const { data: post, status: postStatus, error, isFetching } = useQuery(['post', postId], fetchPost);
+
+  return (
+    <>
+      {postStatus === "loading" ? (
+        <span>Loading post ...</span>
+      ) : postStatus === "error" ? (
+        <span>Error occurred ... {error.message}</span>
+      ) : (
+        <>
+          <a className={styles.link} href="#" onClick={() => setActivePostId()}>&#8592; Back</a>
+          <div>
+            <h6 className={styles.title}>
+              {post.title} {isFetching ? <small>Updating...</small> : null}
+            </h6>
+            <span className={styles.author}>Author - {post.author}</span>
+            <p className={styles.body}>{post.body}</p>
           </div>
         </>
       )}
